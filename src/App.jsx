67c033253bcd66e4a9c65ce40f134be00e041a1e
Nathalie <nathaliebuss21@gmail.com>
2026-04-13 10:56:11 +0200

    fixed media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-// import { useState } from "react";
 import Layout from "./pages/Layout/index";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Characters from "./pages/Characters";
-import Heroes from './pages/Characters/Heroes'
-import Enemies from './pages/Characters/Enemies'
+import Heroes from "./pages/Characters/Heroes";
+import Enemies from "./pages/Characters/Enemies";
 import AllCharacters from "./pages/Characters/AllCharacters";
 import CharacterDetail from "./components/CharacterDetails";
 
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <Routes>
@@ -25,7 +22,10 @@ function App() {
           <Route path="/characters" element={<Characters />}>
             <Route path="/characters/heroes" element={<Heroes />} />
             <Route path="/characters/enemies" element={<Enemies />} />
-            <Route path="/characters/all-characters" element={<AllCharacters/>} />
+            <Route
+              path="/characters/all-characters"
+              element={<AllCharacters />}
+            />
           </Route>
 
           {/* to get right char in searchbar querrie dynamically */}
--- src/components/CharacterDetails/index.jsx
+++ src/components/CharacterDetails/index.jsx
@@ -1,22 +1,30 @@
 import style from "./character-details.module.css";
-import { getImageURL} from '../../utils/function'
+import { getImageURL } from "../../utils/function";
 import { useParams } from "react-router-dom";
 import { getCharacterById } from "../../data/characters";
 
 const CharacterDetail = () => {
-  const { id } = useParams();
-  const character = getCharacterById(id);
+  const { id } = useParams();   // display the right path in the searchbar, taken from app.jsx routes
+  const character = getCharacterById(id);   // it fetches the info from my data file where the info about the character is stored
 
   return (
     <>
       <div className={style.characterDetailContainer}>
         <h1 className={style.name}>{character.name}</h1>
-        <img className={style.img} src={getImageURL(character.image)} alt={character.name} />
+        <img
+          className={style.img}
+          src={getImageURL(character.image)}
+          alt={character.name}
+        />
         <p className={style.charDescription}>{character.description}</p>
         <h3 className={style.powersTitle}>Powers:</h3>
         <p className={style.powers}>
-         {character.powers.map((power, index) => (
-             <li key={index} className={style.powerItem}> {power} </li>))}
+          {character.powers.map((power, index) => (
+            <li key={index} className={style.powerItem}>
+              {" "}
+              {power}{" "}
+            </li>
+          ))}
         </p>
       </div>
     </>
